f30d08f22c4dbeec1425ff516380a64736f8e9b1
Krishna Singh <mail.krishnasingh@gmail.com>
2017-05-23 15:14:27 +0530

    the hero editor completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,34 +1,14 @@
 import { Component } from '@angular/core';
-import { HeroService } from './hero.service';
-import { OnInit } from '@angular/core';
 
-
-export class Hero {
-    id: number;
-    name: string;
-}
 
 
 @Component({
-    selector: 'app-root',
+    selector: 'my-app',
     templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css'],
 })
 
 
 
 export class AppComponent {
     title = 'Tour of Heroes';
-    heroes: Hero[];
-    selectedHero: Hero;
-    onSelect(hero: Hero): void {
-        this.selectedHero = hero;
-    }
-    constructor(private heroService: HeroService) {}
-    getHeroes(): void {
-        this.heroService.getHeroes().then((heroes) => this.heroes = heroes);
-    }
-    ngOnInit(): void {
-        this.getHeroes();
-    }
 };
--- src/app/hero-detail.component.ts
+++ src/app/hero-detail.component.ts
@@ -1,11 +1,39 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Hero } from './hero';
+// Keep the Input import for now, you'll remove it later:
+import { ActivatedRoute, Params } from '@angular/router';
+import { Location } from '@angular/common';
+import { HeroService } from './hero.service';
+import 'rxjs/add/operator/switchMap';
+
+
 
 
 @Component({
     selector: 'hero-detail',
     templateUrl: `./hero-detail.component.html`
 })
-export class HeroDetailComponent {
+export class HeroDetailComponent implements OnInit {
+    constructor(
+        private heroService: HeroService,
+        private route: ActivatedRoute,
+        private location: Location
+    ) {}
+
+    ngOnInit(): void {
+        this.route.params
+            .switchMap((params: Params) => this.heroService.getHero(+params['id']))
+            .subscribe(hero => this.hero = hero);
+    }
     @Input() hero: Hero;
+
+    goBack(): void {
+        this.location.back();
+    }
+
+    save(): void {
+        this.heroService.update(this.hero)
+            .then(() => this.goBack());
+    }
+
 }
